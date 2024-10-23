78a015f52a8942e7d007fe1a4b0c4ff873ea3e00
Ravikumarchavva <Ravikumarchavva>
2024-10-23 11:51:20 +0000

    saving data
    saving data taken from source

--- databricks/startingdata.py
+++ databricks/startingdata.py
@@ -4,7 +4,7 @@ storage_account_secret = dbutils.secrets.get(scope="key-vault-scope", key="twent
 
 # Define the storage account and container details
 storage_account_name = 'twentytwentydata'
-container_name = "sink"
+container_name = "historicaldata"
 
 # Set up the configuration for accessing the storage account
 spark.conf.set(f"fs.azure.account.key.{storage_account_name}.blob.core.windows.net", storage_account_secret)
@@ -12,16 +12,31 @@ spark.conf.set(f"fs.azure.account.key.{storage_account_name}.blob.core.windows.n
 # COMMAND ----------
 
 # Read data from the specified container
-data_path = f"wasbs://{container_name}@{storage_account_name}.blob.core.windows.net/output.csv"
-df = spark.read.format("csv").option("header", "true").load(data_path)
+data_path = f"wasbs://{container_name}@{storage_account_name}.blob.core.windows.net/"
+matches = spark.read.format("csv").option("header", "true").load(data_path+'matches.csv')
 
 # Display the data
-display(df)
+display(matches)
 
 # COMMAND ----------
 
-data = df.select(['match_id','innings','ball','batting_team','bowling_team','venue','winner'])
+deliveries = spark.read.format('csv').option('header','true').load(data_path+'deliveries.csv').drop("venue")
+display(deliveries)
+
+# COMMAND ----------
+
+combined = deliveries.join(matches,'match_id')
+display(combined)
+
+# COMMAND ----------
+
+data = combined.select(['match_id','innings','ball','batting_team','bowling_team','venue','winner'])
 display(data)
+
+# COMMAND ----------
+
+sinkContainer = 'sink'
+data.write.mode('overwrite').option('header','true').csv(f"wasbs://{sinkContainer}@{storage_account_name}.blob.core.windows.net/simpledata")
 
 # COMMAND ----------
 
